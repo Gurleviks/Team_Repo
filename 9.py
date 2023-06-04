31a66fb6396a22f3b580ae379c1a449b436ba2aa
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-06-04 11:49:34 +0300

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,16 @@
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":"land", "monkey":"land", "dog":"land","fish":"water"}
+
+print("Elements of list1:")
+for element in list1:
+    print(element)
+
+print("Elements of tuple1:")
+for element in tuple1:
+    print(element)
+
+print("Elements of set1:")
+for element in set1:
+    print(element)
